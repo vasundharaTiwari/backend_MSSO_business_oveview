e16095449b8db6a25db73cd50b9e3f99ce2d53f4
M20358 <vasundhara4100@gmail.com>
2025-05-30 11:15:14 +0530

    Initial commit

---
+++ src/main/java/com/Msso/MssoBusinessBackend/MssoBusinessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Msso.MssoBusinessBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssoBusinessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssoBusinessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Msso/MssoBusinessBackend/MssoBusinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Msso.MssoBusinessBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssoBusinessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
